9f3c1ffd08853dbaa39f4b2f24c2d2db20e63e0b
iloralobo <israellora85@gmail.com>
2022-11-07 22:58:12 +0100

    añadidas las cartas elegidas a los montones elegidos

--- Cartas/Juego_Cartas_21.py
+++ Cartas/Juego_Cartas_21.py
@@ -22,27 +22,33 @@ def juego_el_21(num_jug):
         print("No es posible repartir cartas de manera equitativa, sólo se aceptan 2,4,5 u 8 jugadores")
     else:
         cartas_repartidas=repartir_cartas(num_jug,int(40/num_jug))
-        print(cartas_repartidas)
+        #print(cartas_repartidas)
 
     # Iniciamos el juego mostrando por consola
     comienzo_partida = " COMIENZA EL JUEGO "
     print(comienzo_partida.center(50, "-"))
-    print(" Montón 1 = " + str(monton_1) + " " + str(puntos_monton) + " puntos ")
-    print(" Montón 2 = " + str(monton_2) + " " + str(puntos_monton) + " puntos ")
-    print(" Montón 3 = " + str(monton_3) + " " + str(puntos_monton) + " puntos ")
-    print(" Montón 4 = " + str(monton_4) + " " + str(puntos_monton) + " puntos ")
+    print("Montón 1 = " + str(monton_1) + " " + str(puntos_monton) + " puntos ")
+    print("Montón 2 = " + str(monton_2) + " " + str(puntos_monton) + " puntos ")
+    print("Montón 3 = " + str(monton_3) + " " + str(puntos_monton) + " puntos ")
+    print("Montón 4 = " + str(monton_4) + " " + str(puntos_monton) + " puntos ")
 
     for jug in range(num_jug):
         print("----------------- TURNO JUGADOR " + str(jug+1) + "-----------------")
         cartas_jugador = cartas_repartidas[jug]
         for cartas in cartas_jugador:
-            print("Tus cartas:" + str(cartas["tipo"]) + cartas["palo"])
+            print("Tus cartas: " + str(cartas["tipo"]) + " " + cartas["palo"])
         carta_elegida = int(input("¿Qué carta quieres poner? "))
         monton_elegido = int(input("¿En qué montón? "))
 
         if monton_elegido == 1:
-            monton_1.append(cartas[jug][carta_elegida]["tipo"]["palo"])
-        print(monton_1)
+            monton_1.append(cartas_jugador[carta_elegida])
+        if monton_elegido == 2:
+            monton_2.append(cartas_jugador[carta_elegida])
+        if monton_elegido == 3:
+            monton_3.append(cartas_jugador[carta_elegida])
+        if monton_elegido == 4:
+            monton_4.append(cartas_jugador[carta_elegida])
+
 
 
 
